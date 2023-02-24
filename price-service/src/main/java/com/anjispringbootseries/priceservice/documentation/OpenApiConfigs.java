df7f9f8d48623b9310d4e7e8f11910e39eb514bd
anjalie.cooray <anjalie.cooray_nh@ntucguest.com>
2023-02-24 20:38:37 +0800

    swagger enabled based on profiles

--- price-service/src/main/java/com/anjispringbootseries/priceservice/documentation/OpenApiConfigs.java
+++ price-service/src/main/java/com/anjispringbootseries/priceservice/documentation/OpenApiConfigs.java
@@ -10,11 +10,13 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
+@Profile({"local", "dev"})
 public class OpenApiConfigs {
   @Bean
   public OpenAPI customOpenAPI(
